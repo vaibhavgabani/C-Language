128b4e4e26a8383abc8ddb50c2e16ea035c07dcc
vaibhav gabani <gabanivaibhav10@gmail.com>
2023-09-28 16:53:30 +0530

    add new loops

--- C-Language/Class work/loop.c
+++ C-Language/Class work/loop.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main(){
-   // 1 type
+   // // 1 type
    for(int i = 1 ; i <= 5 ; i++){
       for(int j = 5 ; j >= i; j--){
-         printf("%d",j);
+         printf("*");
       }
       printf("\n");
    }
    // 2 methord
    for(int q = 5 ; q >= 0 ; q--){
       for(int w = 1 ; w <= q ; w++){
-         printf("%d",w);
+         printf("*");
       }
       printf("\n");
    }
