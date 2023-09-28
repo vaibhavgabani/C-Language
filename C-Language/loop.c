e0fd3b3a0ce0595227c78e35f2d966749a356cb2
vaibhav gabani <gabanivaibhav10@gmail.com>
2023-09-28 14:11:59 +0530

    loop.c move to classwork folder

--- C-Language/loop.c
+++ C-Language/loop.c
@@ -3,14 +3,14 @@ int main(){
    // 1 type
    for(int i = 1 ; i <= 5 ; i++){
       for(int j = 5 ; j >= i; j--){
-         printf("%d",i);
+         printf("%d",j);
       }
       printf("\n");
    }
    // 2 methord
    for(int q = 5 ; q >= 0 ; q--){
       for(int w = 1 ; w <= q ; w++){
-         printf("*");
+         printf("%d",w);
       }
       printf("\n");
    }
