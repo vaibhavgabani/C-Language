a0ae29d0b7156bc3f2097c00a878244e3fba3e9a
vaibhav gabani <gabanivaibhav10@gmail.com>
2023-09-28 17:56:49 +0530

    some new program added

--- C-Language/Class work/tempCodeRunnerFile.c
+++ C-Language/Class work/tempCodeRunnerFile.c
@@ -1,1 +1,10 @@
- printf("*");
+ for(int q = 1 ; q <= 5; q++){
+//     for(int w = 1 ; w<= 5; w++){
+//         if((q+w)>=3){
+//             printf(" ");
+//         } else {
+//             printf("*");
+//         }
+//     }
+//     printf("\n");
+//   }
