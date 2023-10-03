44ae25f8a30963fdbb892b5430db89416049ab9a
vaibhav gabani <gabanivaibhav10@gmail.com>
2023-10-03 21:49:44 +0530

    update 11

--- C-Language/ProblemSheet 2/11_program.c
+++ C-Language/ProblemSheet 2/11_program.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 int main(){
-    int n,r,arm,N;
-    printf("Enter Number :");
-    scanf("%d",&n);
-    N = n;
-    for(int i = 1 ; i <= n ; i++){
-        for(int j = 1 ; j <= i ; j++){
-              r = n % 10;
-              arm = (r*r*r)+r;
-            n = n / 10;
-        }
-        if(N==arm){
-            printf("%d",i);
-        }
+   int max,n,r,ams,sum = 0;
+   printf("Enter Number :");
+   scanf("%d",&max);
+   for(int i = 1 ; i <= max ; i++){
+    n = i;
+    ams = n;
+    sum = 0;
+    while(n!=0){
+        r= n%10;
+        sum = sum+(r*r*r);
+        n= n/10;
     }
+    if(sum == ams){printf("%d\n",ams);}
+   }
     return 0;
 }
