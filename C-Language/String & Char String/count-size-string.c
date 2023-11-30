1f65ca5ca26d865f5f3b6b4c1df831b0136f6c71
vaibhav gabani <gabanivaibhav10@gmail.com>
2023-11-30 19:49:38 +0530

    Deletes

--- C-Language/String & Char String/count-size-string.c
+++ C-Language/String & Char String/count-size-string.c
@@ -4,10 +4,10 @@ int main() {
     char str[50];
     int cnt = 0, temp;
     printf("Enter Value of string: ");
-    scanf(" %[^\n]s", str);
+    scanf("%[^\n]s", str);
 
     for (int i = 0; str[i] != '\0'; i++) {
-        cnt++;
+        cnt++;  
     }
 
     printf("Size you Enter is: %d\n", cnt);
@@ -19,7 +19,15 @@ int main() {
         str[j] = str[i];
         str[i] = temp;
     }
-
+    // you can use bellow loop also
+    // for(int i = 0 ; i < b ; i++){
+    //     for(int j = b-1; j > i ; j--){
+    //         temp = str[j];
+    //         str[j] = str[i];
+    //         str[i] = temp;
+    //         i++;
+    //     }
+    // }
     printf("Reversed string: %s\n", str);
 
     return 0;
