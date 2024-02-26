850ede7669a7a4c9775c35e33472c9e8eeb0c8de
vaibhav gabani <gabanivaibhav10@gmail.com>
2024-02-26 19:11:04 +0530

    major add

--- SEM-2/Pointer/07_2d-array-access-with-single-pointer.c
+++ SEM-2/Pointer/07_2d-array-access-with-single-pointer.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
-int main(){
+
+int main() {
     int arr[3][3] = {
             {1,2,3},
             {4,5,6},
@@ -7,14 +8,20 @@ int main(){
     };
     int *p;
     p = &arr[0][0];
-    //input
-     for(int i = 0 ; i < 3 ; i++){
-        printf("%d ",*(p+i));
-     }
-     //print
-    for(int i = 0 ; i < 3 ; i++){
-        for(int j = 0 ; j < 3 ; j++){
-            printf("arr[%d] address = %u value = %d \n",i,&arr[i][j],arr[i][j]);
+
+    // Input using pointer p
+    printf("Enter elements of the 3x3 array:\n");
+    for(int i = 0 ; i < 3 ; i++) {
+        for(int j = 0 ; j < 3 ; j++) {
+            scanf("%d", p + (i * 3) + j);
+        }
+    }
+
+    // Printing the array
+    printf("\nThe array elements:\n");
+    for(int i = 0 ; i < 3 ; i++) {
+        for(int j = 0 ; j < 3 ; j++) {
+            printf("arr[%d][%d] address = %p value = %d \n", i, j, (void *)&arr[i][j], arr[i][j]);
         }
     }
     return 0;
